f9f7e0913a13624e751fbc2ec9ec5b9b125d7e5b
domiko96 <domiko96@gmail.com>
2018-02-19 23:54:41 +0100

    added cowsay

---
+++ out.py
@@ -0,0 +1,5 @@
+import cowsay
+
+#for i in [‘beavis’, ‘cheese’, ‘daemon’, ‘cow’, ‘dragon’, ‘ghostbusters’, ‘kitty’, ‘meow’, ‘milk’, ‘stegosaurus’, ‘stimpy’, ‘turkey’, ‘turtle’, ‘tux’]
+cowsay.tux("hallo")
+#    pass
