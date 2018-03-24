7c61edea21ef5d525bc8f0640dd8d34a36ee3a64
domiko96 <domiko96@gmail.com>
2018-03-24 19:43:58 +0100

    wrote some js fkt

---
+++ web_page/js/ajax.js
@@ -0,0 +1,36 @@
+function activateLocation(Location_ID){
+        $.ajax({
+            type: 'POST',
+            url: update_location.php,
+            data: {
+                    'Location_ID':Location_ID,
+                    'FromActivateButton' :1
+        };
+        }).done(function(data) {
+                console.log(data)
+        });
+}
+
+
+function resetLocation(Location_ID){
+        $.ajax({
+            type: 'POST',
+            url: update_location.php,
+            data: {
+                    'Location_ID':Location_ID,
+                    'Reset' :1
+        };
+        }).done(function(data) {
+                console.log(data)
+        });
+}
+
+
+function getMapData(Location_ID){
+        $.ajax({
+            type: 'POST',
+            url: map_data.php
+        }).done(function(data) {
+                console.log(data)
+        });
+}
