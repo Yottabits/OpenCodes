4ee1641d0ca1d4ce609543ca91a17001c2ba6786
domiko96 <domiko96@gmail.com>
2018-03-24 20:11:10 +0100

    cleaned up

--- web_page/index.php
+++ web_page/index.php
@@ -9,16 +9,18 @@
 
   <!--Let browser know website is optimized for mobile-->
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+<!--Own JS-->
+<link rel="stylesheet" href="js/ajax.js">
+<link rel="stylesheet" href="js/map.js">
+
 </head>
 
 <body>
   <nav>
     <div class="nav-wrapper grey lighten-3">
       <a href="#!" class="brand-logo left">Logo</a>
-      <!--php: If GET detected --
-      <?php
-              include 'location_scan.php' ;
-      ?>
+      <!--php: If GET detected -->
       <ul class="right">
         <!--javascript: Standby button -->
         <li><a class="waves-effect waves-light btn deep-orange lighten-2">Standby</a></li>
@@ -28,16 +30,17 @@
     </div>
   </nav>
 
-  <!--php: show at first scan (if new session is started)-->
-  <div>
-    <div id="activation-field" class="container">
-      <div class="valign-wrapper row">
-        <div class="col s12 custom-center">
-          <!--TODO: animated activate button-->
-          <a id="activation-button" class="btn-large waves-effect waves-light deep-orange lighten-2"><i class="material-icons">power_settings_new</i></a>
-        </div>
-      </div>
+
+  <?php
+      include 'location_scan.php' ;
+?>
+
+
+
+    <div id="settings", class="fill">
+
     </div>
+
     <div id="footer" class="grey lighten-3">
       <a id="map-button" class="btn-large waves-effect waves-light deep-orange lighten-2" href="index.php">Show map</a>
     </div>
