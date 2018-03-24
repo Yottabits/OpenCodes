baa042bf48b4b4a6b288d877656640b98eaa491d
Max Stabel <max@stabel.family>
2018-03-24 22:09:35 +0100

    Added swiper stylesheet to index

--- web_page/index.php
+++ web_page/index.php
@@ -6,6 +6,7 @@
   <link href="/css/custom.css" rel="stylesheet">
   <!-- Compiled and minified CSS -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.2.0/css/swiper.min.css">
   <!-- Compiled and minified JavaScript -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
