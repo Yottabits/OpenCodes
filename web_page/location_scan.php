6c24aa71fa90e2b53c751a90c440ca4d95f5ac2c
domiko96 <domiko96@gmail.com>
2018-03-24 13:49:10 +0100

    added location_scan file and updated db example

--- web_page/location_scan.php
+++ web_page/location_scan.php
@@ -1,4 +1,14 @@
 <?php
+//check for parameters
+ if(!isset($_GET['UrlCode'])){
+         echo("<h1>No GET UrlCode -> not scanned tag!!</h1> <br>");
+		 echo "ID:  ".$_GET['ID']."<br>";
+ }
+
+//get Parameters to Vars
+$UrlCode = $_GET['UrlCode'];
+
+//connect to db
 $servername = "localhost";
 $username = "yottabytes";
 $password = "fFb80*r1";
@@ -8,9 +18,9 @@ $conn = new mysqli($servername, $username, $password);
 
 // Check connection
 if ($conn->connect_error) {
-        die("DK hats verkackt: " . $conn->connect_error);
+    die("Connection to Database failed: " . $conn->connect_error);
 }else{
-        //echo "Connected successfully";
+   echo "<br>Connected successfully to MYSQLbr>";
 }
 
 $sql = "SELECT * from dk_meet2eat.Locations";
@@ -25,4 +35,6 @@ while ($row = $result->fetch_array(MYSQLI_BOTH)) {
         echo("<h3>Topic: ".$row['Topics_ID']."</h3>");
         echo("<h3>Message: ".$row['Message']."</h3><hr />");
 }
-?>
+
+
+ ?>
