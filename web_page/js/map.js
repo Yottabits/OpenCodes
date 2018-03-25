bc399457fc3a94657a49bf5ca9e2b6d709df2e8c
aewatt95 <andre-eberhard@freenet.de>
2018-03-25 09:02:30 +0200

    Andre chabged a lot

--- web_page/js/map.js
+++ web_page/js/map.js
@@ -1,66 +1,76 @@
+// Some variables we need
+var svg;
+var svgDoc;
+
+function hidePin(pinID){
+  var pin = svgDoc.getElementById("pin" + pinID);
+  pin.style.display = "none";
+}
+
+function showGreenPin(pinID){
+  var pin = svgDoc.getElementById("pin" + pinID);
+  pin.style.display = "";
+  pin.style.fill = "green";
+}
+
+function showRedPin(pinID){
+  var pin = svgDoc.getElementById("pin" + pinID);
+  pin.style.display = "";
+  pin.style.fill = "red";
+}
+
+function setPinStatus(pinID, statusID){
+  if (statusID == 0) {
+    showGreenPin(pinID);
+  }
+  else if (statusID == 1) {
+    showRedPin(pinID);
+  }
+  else {
+    hidePin(pinID);
+  }
+}
+
+//Get the map data from server and update the pins on the map
+function getMapData(){
+  $.ajax({
+    type: 'POST',
+    url: "map_data.php"
+  }).done(function(data) {
+    var obj = JSON.parse(data);
+    for(i = 0; i < obj.length; i++) {
+      setPinStatus(i, obj[i].State);
+    }
+  });
+}
+
+//TODO: repair
+function glowPin(pinID){
+  var pin = svgDoc.getElementById("pin" + pinID);
+  pin.style.height = "100px";
+}
+
+function refresh(){
+  getMapData();
+}
+
 $( document ).ready(function(){
-    // Some variables we need
-    var svg = document.getElementById("svgContainer");
-    var svgDoc = svg.contentDocument;
+  svg = document.getElementById("svgContainer");
+  svgDoc = svg.contentDocument;
 
-    function hidePin(pinID){
-        var pin = svgDoc.getElementById("pin" + pinID);
-        pin.style.display = "none";
-    }
-    
-    function showGreenPin(pinID){
-        var pin = svgDoc.getElementById("pin" + pinID);
-        pin.style.display = "";
-        pin.style.fill = "green";
-    }
-    
-    function showRedPin(pinID){
-        var pin = svgDoc.getElementById("pin" + pinID);
-        pin.style.display = "";
-        pin.style.fill = "red";
-    }
+  // Hide all pins by default
+  for (i = 0; i < 4; i++){
+    hidePin(i);
+  }
+  // Show the svg
+  svg.style.display = "";
 
-    function setPinStatus(pinID, statusID){
-        if (statusID == 0) {
-            showGreenPin(pinID);
-        }
-        else if (statusID == 1) {
-            showRedPin(pinID);
-        }
-        else {
-            hidePin(pinID);
-        }
-    }
+  ///////////////////////////////////////////////////////////////////////////
 
-    // Hide all pins by default
-    for (i = 0; i < 4; i++){
-        hidePin(i);
-    }
-    
-    // Show the svg
-    svg.style.display = "";
+  getMapData();
+  setInterval(refresh, 5000);
 
-    ///////////////////////////////////////////////////////////////////////////
 
-    //Get the map data from server and update the pins on the map
-    function getMapData(){
-        $.ajax({
-            type: 'POST',
-            url: "map_data.php"
-        }).done(function(data) {
-                var obj = JSON.parse(data);
-            for(i = 0; i < obj.length; i++) {
-                setPinStatus(i, obj[i].State);
-            }
-        });
-    }
+  // Automatically get new data every 5 sec
 
-    // Automatically get new data every 5 sec
-    function refresh(){
-        getMapData();
-    }
-
-    getMapData();
-    setInterval(refresh, 5000);
 })
-
--- web_page/tv_mode.php
+++ web_page/tv_mode.php
@@ -17,7 +17,7 @@
     <div class="col s8 fill">
       <div class="card" style="height: 100%">
         <div class="mapView card-content">
-          <object id="svgContainer" type="image/svg+xml" data="../web_page/img/map_with_pins.svg" style="width: 100%; height: auto;"></object>
+          <object id="svgContainer" type="image/svg+xml" data="/img/map_with_pins.svg" style="width: 100%; height: auto;"></object>
         </div>
       </div>
     </div>
@@ -25,40 +25,43 @@
       <div class="card white">
         <div class="card-content black-text">
           <span class="card-title">
-            <h3>name: </h3><br /><h2 id="userName">lonelyMensaEater69</h1><br /><hr />
-              <h3>topic: </h3><br /><h2 id="topic">Backgamon</h2><hr />
-              <h3>message:</h3><br /><h2 id="message">I like backgamon. What about you?</h2>
+            <h3>name: </h3><br /><h2 id="WhoIsHere">lonelyMensaEater69</h1><br /><hr />
+              <h3>topic: </h3><br /><h2 id="Topic">Backgamon</h2><hr />
+              <h3>message:</h3><br /><h2 id="Message">I like backgamon. What about you?</h2><hr />
+              <h3>free seats:</h3><br /><h2 id="Seats">3</h2>
             </span>
           </div>
         </div>
         <div class="card horizontal">
           <div class="card-image">
-              <object id="svgContainer" type="image/svg+xml" data="../web_page/img/map_with_pins.svg" style="width: 20%; height: auto;"></object>
+
           </div>
           <div class="card-stacked">
-          <div class="card-content">
-            <h3>More information at</h3>
+            <div class="card-content">
+              <h3>More information at</h3>
             </div>
             <div class="card-action">
               <h2>www.unsereURL.de</h2>
             </div>
           </div>
-          </div>
-
         </div>
+
       </div>
-      <!-- Compiled and minified JavaScript -->
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.2.0/js/swiper.min.js"></script>
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-      <script src="/js/map.js"></script>
-      <script>
-      var swiper = new Swiper('.swiper-container', {
-        scrollbar: {
-          el: '.swiper-scrollbar',
-          hide: true,
-        },
-      });
-      </script>
-    </body>
-    </html>
+    </div>
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.2.0/js/swiper.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="js/mapTv.js"></script>
+    <script>
+    var swiper = new Swiper('.swiper-container', {
+      scrollbar: {
+        el: '.swiper-scrollbar',
+        hide: true,
+      },
+    });
+
+
+    </script>
+  </body>
+  </html>
